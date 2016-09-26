54381ff6d6c91814edb278f16a022e9c692b6b94
Lee Squall <jaffli@yahoo.com.tw>
2016-09-26 11:15:09 +0800

    Initial Commit

---
+++ gridTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gridTest
+//
+//  Created by 宗桓 李 on 2016/9/26.
+//  Copyright © 2016年 squall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
